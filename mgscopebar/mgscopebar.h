548449c0d26dac7fe3961d2600a22d26c1289757
Marten Payne <m.payne@qsr.com.au>
2012-10-10 18:03:24 +1100

    More framework related cleanups

---
+++ mgscopebar/mgscopebar.h
@@ -0,0 +1,13 @@
+//
+//  mgscopebar.h
+//  mgscopebar
+//
+//  Created by Marten Payne on 10/10/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mgscopebar : NSObject
+
+@end
---
+++ mgscopebar/mgscopebar.m
@@ -0,0 +1,13 @@
+//
+//  mgscopebar.m
+//  mgscopebar
+//
+//  Created by Marten Payne on 10/10/12.
+//
+//
+
+#import "mgscopebar.h"
+
+@implementation mgscopebar
+
+@end
